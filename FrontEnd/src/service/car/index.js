cf0d90d1bad21b267249d2f81490cdef770c9d59
Joe Ferdinan <119273161+JoF24@users.noreply.github.com>
2024-11-14 20:26:35 +0700

    Solve Bug Car and Model (#16)
    * Solve Bug Car and Model

--- FrontEnd/src/service/car/index.js
+++ FrontEnd/src/service/car/index.js
@@ -35,7 +35,7 @@ export const getDetailCars = async (id) => {
     return result;
 };
 
-export const CreateCars = async (request) => {
+export const createCars = async (request) => {
     const token = localStorage.getItem("token");
 
     const formData = new FormData();
@@ -71,8 +71,21 @@ export const updateCars = async (id, request) => {
     const token = localStorage.getItem("token");
 
     const formData = new FormData();
-    formData.append("manufacture", request.manufacture);
-    formData.append("address", request.address);
+    formData.append("cars", request.cars);
+    formData.append("plate", request.plate);
+    formData.append("manufacture_id", request.manufacture_id);
+    formData.append("model_id", request.model_id);
+    formData.append("rentPerDay", request.rentPerDay);
+    formData.append("capacity", request.capacity);
+    formData.append("description", request.description);
+    formData.append("availableAt", request.availableAt);
+    formData.append("transmission_id", request.transmission_id);
+    formData.append("available", request.available);
+    formData.append("type_id", request.type_id);
+    formData.append("year", request.year);
+    formData.append("options", request.options);
+    formData.append("specs", request.specs);
+    formData.append("fuel_id", request.fuel_id);
 
     const response = await fetch(`${import.meta.env.VITE_API_URL}/cars/${id}`, {
         headers: {
